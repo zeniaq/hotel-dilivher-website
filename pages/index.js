ac91146b1567a84efc090bdb63b250e941d793fc
Erick Sarabia <ecksarabia@gmail.com>
2020-10-08 09:34:56 -0500

    Add button component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
+import Button from "../components/Button";
+
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
     return (
         <div className={styles.container}>
             <h1>Hotel Dilivher</h1>
+            <Button className={styles.button}>Enviar Mensaje</Button>
         </div>
     );
 }
